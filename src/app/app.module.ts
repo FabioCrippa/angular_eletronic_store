e6d1c0ac99116d66c521c479a2315ca01d30059c
FabioCrippa <crippa.fabiocrippa@gmail.com>
2022-09-06 15:00:17 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { CartComponent } from './pages/cart/cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    CartComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imagem: string;
+}
+
+export const products = [
+    {id: 1, name: "Galaxy S20", price: 899, description: "A large phone with one of the best screens.", imagem: "./assets/Iphone.jpg" },
+    {id: 2, name: "Nokia Z40", price: 699, description: "A great phone. The best cameras.", imagem: "./assets/Iphone.jpg" },
+    {id: 3, name: "Samsung", price: 899, description: "A large phone. Very fast.", imagem: "./assets/Iphone.jpg" },
+    {id: 4, name: "Galaxy Z Flip4", price: 459, description: "The most versatile camera phone.", imagem: "./assets/Iphone.jpg" },
+    {id: 5, name: "Galaxy Z Fold4", price: 1299, description: "Make the most of your day.", imagem: "./assets/Iphone.jpg" },
+    {id: 6, name: "Galazy S22", price: 1399, description: "Designed for those who want to capture.", imagem: "./assets/Iphone.jpg" },
+    {id: 7, name: "Galazy S22", price: 1399, description: "Designed for those who want to capture.", imagem: "./assets/Iphone.jpg" },
+    {id: 8, name: "Galazy S22", price: 1399, description: "Designed for those who want to capture.", imagem: "./assets/Iphone.jpg" },
+    {id: 9, name: "Galazy S22", price: 1399, description: "Designed for those who want to capture.", imagem: "./assets/Iphone.jpg" },
+    {id: 10, name: "Galazy S22", price: 1399, description: "Designed for those who want to capture.", imagem: "./assets/Iphone.jpg" },
+    {id: 11, name: "Galazy S22", price: 1399, description: "Designed for those who want to capture.", imagem: "./assets/Iphone.jpg" },
+    {id: 12, name: "Galazy S22", price: 1399, description: "Designed for those who want to capture.", imagem: "./assets/Iphone.jpg" },
+]
